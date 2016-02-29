d6031cf24ff7d09f0a6891ba22f1a253550e09c3
Yannick Chabbert <yannick.chabbert@gmail.com>
2016-02-29 14:55:07 +0100

    use sourcemap instead

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     minifyCss = require('gulp-minify-css'),
     concat = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps'),
     util = require('gulp-util'),
     gettext = require('gulp-angular-gettext'),
     ngAnnotate = require('gulp-ng-annotate'),
@@ -10,50 +11,18 @@ var gulp = require('gulp'),
     fs = require('fs'),
     config = require('./config.js');
 
-gulp.task('jshint', function() {
-  return gulp.src('src/js/**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
+gulp.task('js', ['jshint', 'config', 'templatecache', 'translations'], function () {
+  var sourcemap = config.sourcemap.vendor;
+  sourcemap = sourcemap.concat(['build/js/config.js']);
+  sourcemap = sourcemap.concat(config.sourcemap.mymoney);
+  sourcemap = sourcemap.concat(['build/js/templates.js']);
 
-gulp.task('config', function() {
-  var path = ['build', 'build/js'];
-  for (var i = 0; i < path.length; i++) {
-    try {
-      fs.statSync(path[i]);
-    } catch (err) {
-      fs.mkdirSync(path[i]);
-    }
-  }
-  var content = 'var MyMoneyConfig = ' + JSON.stringify(config, null, 2) + ';\n';
-  fs.writeFileSync('build/js/config.js', content);
-});
-
-gulp.task('app', ['config', 'templatecache'], function() {
-  var map = ['build/js/config.js'];
-  map = map.concat(config.sourcemap.mymoney);
-  map = map.concat(['build/js/templates.js']);
-
-  return gulp.src(map)
-    .pipe(concat('app.js'))
-    .pipe(ngAnnotate({single_quotes: true}))
-    .pipe(gulp.dest('build/js/'));
-});
-
-gulp.task('vendor', function() {
-  return gulp.src(config.sourcemap.vendor)
-    .pipe(concat('vendor.js'))
-    .pipe(gulp.dest('build/js/'));
-});
-
-gulp.task('js', ['jshint', 'vendor', 'app', 'translations'], function () {
-  var sourcemap = [
-    'build/js/vendor.js',
-    'build/js/app.js'
-  ];
   gulp.src(sourcemap)
+    .pipe(sourcemaps.init())
+    .pipe(ngAnnotate({single_quotes: true}))
     .pipe(concat('mymoney.min.js'))
     .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist/js/'));
 
   if (util.env.locale) {
@@ -79,8 +48,10 @@ gulp.task('css', function () {
   return gulp.src([
     'src/css/styles.css'
   ])
+  .pipe(sourcemaps.init())
   .pipe(concat('mymoney.min.css'))
   .pipe(minifyCss())
+  .pipe(sourcemaps.write())
   .pipe(gulp.dest('dist/css/'));
 });
 
@@ -101,6 +72,32 @@ gulp.task('translations', function () {
   .pipe(gulp.dest('build/js/locales'));
 });
 
+gulp.task('watch', function () {
+  // Be careful, environment variable are not persistent (locale flag ignored).
+  gulp.watch(['src/js/*/**.js'], ['js', 'pot']);
+  gulp.watch(['src/css/*/**.css'], ['css']);
+  gulp.watch(['src/partials/**/*.html'], ['pot', 'templatecache', 'js']);
+});
+
+gulp.task('jshint', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('config', function() {
+  var path = ['build', 'build/js'];
+  for (var i = 0; i < path.length; i++) {
+    try {
+      fs.statSync(path[i]);
+    } catch (err) {
+      fs.mkdirSync(path[i]);
+    }
+  }
+  var content = 'var MyMoneyConfig = ' + JSON.stringify(config, null, 2) + ';\n';
+  fs.writeFileSync('build/js/config.js', content);
+});
+
 gulp.task('templatecache', function () {
   return gulp.src(
     'src/partials/**/*.html'
@@ -111,13 +108,6 @@ gulp.task('templatecache', function () {
       root: config.templatePath
     }))
     .pipe(gulp.dest('build/js'));
-});
-
-gulp.task('watch', function () {
-  // Be careful, environment variable are not persistent (locale flag ignored).
-  gulp.watch(['src/js/*/**.js'], ['js', 'pot']);
-  gulp.watch(['src/css/*/**.css'], ['css']);
-  gulp.watch(['src/partials/**/*.html'], ['pot', 'templatecache', 'js']);
 });
 
 gulp.task('default', ['js', 'css']);
